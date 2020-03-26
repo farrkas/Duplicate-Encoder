da7398ca3231ec87b9056179797faf8cb125c2a1
Maciek Lorencowicz <m.lorencowicz@gmail.com>
2020-03-26 18:57:38 +0100

    przeszło testy

--- dup.js
+++ dup.js
@@ -1,42 +1,30 @@
 https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/train/javascript
 
-
 function duplicateEncode(word) {
     const nawiasy = ["(", ")"];
-
     var wordArr = [...word];
     wordArr = wordArr.map(el => el.toUpperCase());
-    wordArr.sort();
-
+    //wordArr.sort();
     var wynik = [];
     var powtorzone = [];
-    //   wordArr.forEach((el) => {if (!powtorzone.includes(el)) powtorzone.push(el)});
-    //   var powtorzone = wordArr.map((el) => {
-    //       if 
+
     wordArr.forEach((el) => {
-        if (wordArr.lastIndexOf(el) - wordArr.indexOf(el) != 0) { powtorzone.push(el) };
+        if (wordArr.lastIndexOf(el) != wordArr.indexOf(el) && !powtorzone.includes(el)) { powtorzone.push(el) };
 
     });
 
-
-    var k = 0;
+    //let k = 0;
     wynik = wordArr.map((el) => {
         if (powtorzone.includes(el)) {
-            k = Math.abs(k - 1);
-            return nawiasy[k];
-
+            //k = Math.abs(k - 1);
+            return nawiasy[1];
         }
-        else return nawiasy[k];
+        else return nawiasy[0];
     });
-
-
-
     console.log(wordArr);
     console.log(powtorzone);
     console.log(wynik.join(""));
 }
 
-
-
 duplicateEncode("din"); //"((("
-duplicateEncode("Agata"); //"((("
+duplicateEncode("recede"); //"()()()"
